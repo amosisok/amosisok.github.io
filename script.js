df891b8dd67a344925745e9000bc4e3fe8513cd0
Amos <amosisok@gmail.com>
2022-02-25 12:42:07 -0800

    Update projects

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 function links() {
 let aboutLink = document.getElementById("aboutLink");
 let about = document.getElementById("about");
+
+let projectsLink = document.getElementById("projectsLink");
+let projects = document.getElementById("projects");
+
 aboutLink.addEventListener("click", function() {
     about.scrollIntoView({
         behavior: "smooth",
         block: "start",
         inline: "nearest"
     });
-})
+});
+
+projectsLink.addEventListener("click", function() {
+    projects.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+        inline: "nearest"
+    });
+});
+
 }
 
 links();
